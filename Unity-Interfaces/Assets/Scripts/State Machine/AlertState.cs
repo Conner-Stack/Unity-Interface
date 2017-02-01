24b09a20fc149aad30e843ea3c0e735dbd7fc175
Conner-Stack <connerstack@gmail.com>
2017-02-01 15:14:52 -0600

    Unity-Interface push
    Working on Unity tutorial for using interfaces and FiniteStateMachines

---
+++ Unity-Interfaces/Assets/Scripts/State Machine/AlertState.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlertState : IEnemyState {
+    public void OnTriggerEnter(Collider other)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ToAlertState()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ToChaseState()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ToPatrolState()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Use this for initialization
+
+    void IEnemyState.Update()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Update is called once per frame
+
+}
---
+++ Unity-Interfaces/Assets/Scripts/State Machine/StatePattern.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class StatePattern : MonoBehaviour {
+
+    public float searchingTurnSpeed = 120f;
+    public float searchingDuration = 4f;
+    public float sightRange = 20f;
+    public Transform[] wayPoints;
+    public Transform eyes;
+    public Vector3 offset = new Vector3(0, .5f, 0);
+    public MeshRenderer meshRendererFlag;
+
+    [HideInInspector]
+    public Transform chaseTarget;
+    [HideInInspector]
+    public IEnemyState currentState;
+    [HideInInspector]
+    public ChaseState chaseState;
+    [HideInInspector]
+    public AlertState alertState;
+    [HideInInspector]
+    public PatrolState patrolState;
+    [HideInInspector]
+    public NavMeshAgent navMeshAgent;
+
+
+
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
